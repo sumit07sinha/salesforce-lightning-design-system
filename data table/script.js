73ccc7d3f7790f0596b82f433272902dbae5f03b
Sumit Sinha <51187812+sumit07sinha@users.noreply.github.com>
2020-02-28 11:29:08 +0530

    Update script.js

--- data table/script.js
+++ data table/script.js
@@ -1,21 +1,25 @@
 
 function sortTable() {
-    let table, rows, switching, i, x, y, shouldSwitch;
+    let table, rows, i,shouldSwitch;
     table = document.getElementById("sTable");
-    switching = true;
+    let switching = true;
 
     while (switching) {
       switching = false;
       rows = table.rows;
+      
       for (i = 1; i < (rows.length - 1); i++) {
         shouldSwitch = false;
-        
-        x = rows[i].getElementsByTagName("TD")[0];
-        y = rows[i + 1].getElementsByTagName("TD")[0];
-        //checking if the two rows should switch place:
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        let rowsTD = rows[i].getElementsByTagName("TD")[0];
+        let rowsNextTD = rows[i + 1].getElementsByTagName("TD")[0];
+        //check if the two rows should switch place:
+        if (rowsTD.innerHTML.toLowerCase() > rowsNextTD.innerHTML.toLowerCase()) {
           shouldSwitch = true;
           break;
+        }
+        else{
+            shouldSwitch = false;
+            break;  
         }
       }
       if (shouldSwitch) {
